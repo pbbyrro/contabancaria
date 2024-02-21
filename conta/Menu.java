2fba8a80382663941c64e4aca99ad873c4fd764f
Patricia Byrro <pbbyrro@gmail.com>
2024-02-20 21:39:37 -0300

    Adicionar menu e conta

--- conta/Menu.java
+++ conta/Menu.java
@@ -1,10 +1,26 @@
 package conta;
 
+import conta.model.Conta;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Conta c1 = new Conta(23, 555, 1, "Patricia", 1000f);
 
+		c1.visualizar();
+
+		if (c1.sacar(100))
+			System.out.println("Saque efetuado com sucesso. O novo saldo é de: " + c1.getSaldo());
+		else
+			System.out.println("O saldo é insuficiente!");
+		
+		
+		c1.depositar(1000);
+		System.out.println("Depósito efetuado com sucesso. O novo saldo é de: " + c1.getSaldo());
+		
+
+		
 	}
 
 }
